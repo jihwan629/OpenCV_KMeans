55facee7bdb901f4b1b9033a737f17811d61e850
jihwan629 <jihwan629@naver.com>
2021-01-20 13:23:03 +0900

    초기 커밋.

---
+++ KMeans/KMeans.cpp
@@ -0,0 +1,22 @@
+﻿#include <opencv2/imgcodecs.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+    printf("OpenCV Version : %s\n\n", CV_VERSION);
+
+	//Mat img = imread("Lenna.png", CV_LOAD_IMAGE_UNCHANGED);
+	Mat img = imread("input.png", CV_LOAD_IMAGE_UNCHANGED);
+
+	imshow("img", img);
+	waitKey(0);
+
+	return 0;
+}
