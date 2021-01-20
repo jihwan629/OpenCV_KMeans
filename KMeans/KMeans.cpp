ec1c49f17991368dc8afa1a24e6fa257bd5eb1a5
jihwan629 <jihwan629@naver.com>
2021-01-20 14:59:50 +0900

    오차 제곱합 구하기

--- KMeans/KMeans.cpp
+++ KMeans/KMeans.cpp
@@ -25,7 +25,9 @@ Scalar colorTab[] =
 };
 
 Mat img, res, points, labels, centers;
-int Width, Height, Pixels, K;
+int Width, Height, Pixels, Knum;
+
+unsigned int *SSD;
 
 int main()
 {
@@ -39,14 +41,15 @@ int main()
 		return -1;
 	}
 	
-	Width = img.cols;				//이미지 너비 설정
-	Height = img.rows;				//이미지 높이 설정
-	Pixels = Width * Height;		//이미지의 전체 pixel값 설정
-	K = 3;							//클러스터링 개수
+	Width = img.cols;						//이미지 너비 설정
+	Height = img.rows;						//이미지 높이 설정
+	Pixels = Width * Height;				//이미지의 전체 pixel값 설정
+	Knum = 8;								//클러스터링 개수
+	SSD = new unsigned int [Knum + 1] {0};	//클러스터링 개수마다 오차제곱합
 
 	points.create(Pixels, 1, CV_32FC3);
 	res.create(Height, Width, img.type());
-	centers.create(K, 1, CV_32FC3);
+	centers.create(Knum, 1, CV_32FC3);
 
 	// 채널 값 변경
 	for (int y = 0, n = 0; y < Height; y++) {
@@ -60,20 +63,37 @@ int main()
 		}
 	}
 
-	// kmeans
-	kmeans(points, K, labels, 
-		TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 10, 1.9),
-		3, KMEANS_RANDOM_CENTERS, centers);
+	for (int k = 1; k <= Knum; k++)
+	{
+		// kmeans
+		kmeans(points, k, labels,
+			TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 10, 1.9),
+			3, KMEANS_RANDOM_CENTERS, centers);
 
-	for (int y = 0, n = 0; y < Height; y++) {
-		for (int x = 0; x < Width; x++, n++) {
-			//현재 픽셀의 label값을 받아온다.
-			int cIndex = labels.at<int>(n);
-			//받아온 label값에 대응되는 색상을 입힌다.
-			res.at<Vec4b>(y, x)[0] = colorTab[cIndex][0];
-			res.at<Vec4b>(y, x)[1] = colorTab[cIndex][1];
-			res.at<Vec4b>(y, x)[2] = colorTab[cIndex][2];
+		for (int y = 0, n = 0; y < Height; y++) {
+			for (int x = 0; x < Width; x++, n++) {
+				//현재 픽셀의 label값을 받아온다.
+				int cIndex = labels.at<int>(n);
+				//받아온 label값에 대응되는 색상을 입힌다.
+				res.at<Vec4b>(y, x)[0] = colorTab[cIndex][0];
+				res.at<Vec4b>(y, x)[1] = colorTab[cIndex][1];
+				res.at<Vec4b>(y, x)[2] = colorTab[cIndex][2];
+			}
 		}
+
+		// Elbow 기법
+
+		// SSD (오차 제곱합) 구하기
+		for (int y = 0, n = 0; y < Height; y++) {
+			for (int x = 0; x < Width; x++, n++) {
+				SSD[k] += norm(points.at<Vec3f>(n) - centers.at<Vec3f>(labels.at<int>(n), 0));
+			}
+		}
+	}
+	
+	// SSD (오차 제곱합) 구하기
+	for (int i = 1; i <= Knum; i++) {
+		cout << i << " : " << SSD[i] << endl;
 	}
 
 	namedWindow("Input", WINDOW_AUTOSIZE);
@@ -83,6 +103,9 @@ int main()
 	imshow("Result", res);
 
 	waitKey(0);
+
+	// 배열 할당 해제
+	delete[] SSD;
 
 	return 0;
 }
